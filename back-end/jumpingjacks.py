5e11d6349f936ab8e9dc9e4e50c527940cdfa544
Sanjay Chandrasekar <sanjay.chandrasekar@gmail.com>
2023-04-08 17:14:07 -0700

    jumpingjacks detection

--- back-end/jumpingjacks.py
+++ back-end/jumpingjacks.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import PoseModule
 
-cap = cv2.VideoCapture("jumping-jacks.mp4")
+cap = cv2.VideoCapture('jumping-jacks.mp4')
 
 detector = PoseModule.poseDetector()
 
@@ -18,7 +18,7 @@ while True:
     if len(landmarks) != 0:
         # Right Arm
         RarmAngle = detector.findAngle(frame, 12, 14, 16)
-        # # Left Arm
+        # Left Arm
         LarmAngle = detector.findAngle(frame, 11, 13, 15)
 
         # Right Leg
@@ -26,13 +26,12 @@ while True:
         # Left Leg
         LlegAngle = detector.findAngle(frame, 23, 25, 27)
 
-        
         # percentage = np.interp(LarmAngle, (60, 130), (0, 100))
 
-        if direction == "up" and LarmAngle < 110 and RarmAngle < x:
+        if direction == "up" and LarmAngle < 110 and RarmAngle > 235 and LlegAngle < 182.5 and RlegAngle > 190.5:
             repCount += 0.5
             direction = "down"
-        elif direction == "down" and LarmAngle > 170 and RarmAngle > x:
+        elif direction == "down" and LarmAngle > 170 and RarmAngle < 190 and LlegAngle > 171.5 and RlegAngle < 179.5:
             repCount += 0.5
             direction = "up"
         
