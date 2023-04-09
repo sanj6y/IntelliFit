15db4306d7131b3c25a426c0bbdc9a1bea6b98e5
anuragchillarige <pinganurag@gmail.com>
2023-04-08 22:48:16 -0700

    settings.jsx and signin.jsx functional

--- front-end/src/Pages/Settings.jsx
+++ front-end/src/Pages/Settings.jsx
@@ -1,14 +1,86 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Sidebar from '../Components/Sidebar';
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db, logout } from "../firebase";
+import { collection, query, onSnapshot, where, getDocs, orderBy, deleteDoc, doc, updateDoc } from "firebase/firestore";
+import { deleteUser, updateEmail } from "firebase/auth";
+
+import { useNavigate } from 'react-router-dom';
+
 
 import '../Styles/Settings.css'
 
 export default function Settings() {
 
-    const nameRef = useRef(null)
+    const nav = useNavigate();
 
-    const passRef = useRef(null)
-    const passConfirmRef = useRef(null)
+    const [currUser, loading] = useAuthState(auth)
+    const [user, setUser] = useState('')
+    const [userDoc, setUserDoc] = useState()
+    const [name, setName] = useState('');
+
+    const nameRef = useRef(null)
+    const emailRef = useRef(null)
+
+    const currentUser = auth.currentUser
+
+    const changeEmail = async () => {
+
+        try {
+            await updateEmail(currentUser, emailRef.current.value)
+        }
+
+        catch (e) {
+            console.error("error", e);
+            alert("Error: Could not update email.")
+        }
+
+        emailRef.current.value = ''
+    }
+
+    const changeName = async () => {
+        const docRef = doc(db, "users", user)
+        try {
+            await updateDoc(docRef, { name: nameRef.current.value + "" })
+            setName(nameRef.current.value + "");
+        } catch (e) {
+            alert("Error: Could not change name")
+            console.log(e)
+
+        }
+        nameRef.current.value = ''
+    }
+
+    const deleteAccount = async () => {
+        if (window.confirm("Are you sure you want to delete your account?")) {
+            try {
+                await deleteUser(currentUser)
+                nav("/")
+
+            } catch (e) {
+                alert("Error: Could not delete account")
+                console.log(e)
+            }
+        }
+    }
+
+    useEffect(() => {
+        const usr = async () => {
+            if (currUser) {
+                const q = query(
+                    collection(db, 'users'),
+                    where('uid', '==', currUser?.uid)
+                );
+                const userDoc = await getDocs(q);
+                setUserDoc(userDoc.docs[0])
+                setUser(userDoc.docs[0].id)
+                setName(userDoc.docs[0].data().name)
+            }
+        }
+        usr()
+
+    }, [currUser])
 
     return (
         <div className="settings-holder">
@@ -17,22 +89,23 @@ export default function Settings() {
                 <div className="name-change">
                     <h2>Name</h2>
                     <p>Change your current display name.</p>
-                    <form action="">
-                        <input type="text" placeholder='Enter New Name' />
+                    <form onSubmit={e => { e.preventDefault(); changeName() }}>
+                        <input type="text" placeholder='Enter New Name' ref={nameRef} />
                     </form>
                 </div>
-                <div className="change-pw">
-                    <h2>Password</h2>
-                    <p>Change your current password</p>
+                <div className="change-email">
+                    <h2>Email</h2>
+                    <p>Change your current email</p>
 
-                    <form action="">
-                        <input type="password" placeholder='New Password' />
-                        <input type="password" placeholder='Confirm Password' />
+                    <form onSubmit={(e) => { e.preventDefault(); changeEmail() }}>
+                        <input type="email" placeholder='New Email' ref={emailRef} />
 
                         <input type="submit" />
                     </form>
 
                 </div>
+
+                <button onClick={() => deleteAccount()}>Delete Account</button>
             </div>
         </div>
     )
--- front-end/src/Pages/SignIn.jsx
+++ front-end/src/Pages/SignIn.jsx
@@ -9,61 +9,61 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth, login } from '../firebase';
 import '../Styles/SignIn.css';
 function SignIn() {
-  const [email, setEmail] = useState('');
-  const [pass, setPass] = useState('');
-  const [currUser, loading, err] = useAuthState(auth);
-  const nav = useNavigate();
-  useEffect(() => {
-    if (loading) {
-      // future implementation of loading screen
-      return;
-    }
-    // if (currUser) nav('/dash');
-  }, [currUser, loading]);
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+    const [currUser, loading, err] = useAuthState(auth);
+    const nav = useNavigate();
+    useEffect(() => {
+        if (loading) {
+            // future implementation of loading screen
+            return;
+        }
+        if (currUser) nav('/dashboard');
+    }, [currUser, loading]);
 
-  return (
-    <div className="holder">
-      <div className="welcome-panel">
-        <div className="welcome">
-          <div className="title">
-            <img src={logo} width={85} height={55}/>
-          </div>
-          <div className='title-holder-holder'>
-            <h2>
-              Sign in
-            </h2>
-            <hr/>
-          </div>
-          <input
-            value={email}
-            placeholder="E-mail"
-            type="email"
-            className="email"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            value={pass}
-            onChange={(e) => setPass(e.target.value)}
-            placeholder="Password"
-            type="password"
-            className="password"
-          />
-          <button className="login" onClick={() => login(email, pass)}>
-            Login
-          </button>
-          <div className='holder-link-holder-holder'>
-            <Link to="/signup" className="sign-up-button">
-                <span>Sign up</span>
-              </Link>
-            <Link to="/reset" className="forgot-password">
-              <span>Forgot password?</span>
-            </Link>
-          </div>
+    return (
+        <div className="holder">
+            <div className="welcome-panel">
+                <div className="welcome">
+                    <div className="title">
+                        <img src={logo} width={85} height={55} />
+                    </div>
+                    <div className='title-holder-holder'>
+                        <h2>
+                            Sign in
+                        </h2>
+                        <hr />
+                    </div>
+                    <input
+                        value={email}
+                        placeholder="E-mail"
+                        type="email"
+                        className="email"
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <input
+                        value={pass}
+                        onChange={(e) => setPass(e.target.value)}
+                        placeholder="Password"
+                        type="password"
+                        className="password"
+                    />
+                    <button className="login" onClick={() => login(email, pass)}>
+                        Login
+                    </button>
+                    <div className='holder-link-holder-holder'>
+                        <Link to="/signup" className="sign-up-button">
+                            <span>Sign up</span>
+                        </Link>
+                        <Link to="/reset" className="forgot-password">
+                            <span>Forgot password?</span>
+                        </Link>
+                    </div>
+                </div>
+
+            </div>
         </div>
-
-      </div>
-    </div>
-  );
+    );
 }
 
 export default SignIn;
