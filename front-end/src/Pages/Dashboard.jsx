0b9089838e0473a108781aac503fa92f5178a026
bhavan060 <70976789+bhavan060@users.noreply.github.com>
2023-04-09 05:05:52 -0700

    bsdoifbsf

--- front-end/src/Pages/Dashboard.jsx
+++ front-end/src/Pages/Dashboard.jsx
@@ -29,8 +29,7 @@ export default function Dashboard() {
 
     const nav = useNavigate();
     const [name, setName] = useState("")
-    const [lastWorkouts, setLastWorkouts] = useState(['workout1', 'workout2', 'workout3', 'workout4'])
-
+    const [allOfWorkouts, setAllOfWorkouts] = useState([])
     const [allWorkouts, setAllWorkouts] = useState([])
     const [currUser, loading] = useAuthState(auth)
     const [user, setUser] = useState('')
@@ -44,6 +43,28 @@ export default function Dashboard() {
             const doc = await getDocs(q);
             const data = doc.docs[0].data();
             setName(data.name);
+        } catch (err) {
+            alert('An error had occurred while fetching the users name');
+            console.log(err)
+        }
+    };
+
+    const fetchDefaultWorkouts = async () => {
+        try {
+            const q = query(
+                collection(db, 'users'),
+                where('uid', '==', currUser?.uid)
+            );
+            const doc = await getDocs(q);
+            const data = doc.docs[0].data();
+
+            let ex1 = {"exercises": data.exercises1, "name": data.name1, "lastUsed":data.lastUsed1};
+            let ex2 = {"exercises": data.exercises2, "name": data.name2, "lastUsed":data.lastUsed2};
+            let ex3 = {"exercises": data.exercises1, "name": data.name1, "lastUsed":data.lastUsed1};
+            let arr = [ex1, ex2, ex3]
+            setAllOfWorkouts([...arr])
+
+            
         } catch (err) {
             alert('An error had occurred while fetching the users name');
             console.log(err)
@@ -64,73 +85,61 @@ export default function Dashboard() {
         return arr;
     }
 
+
     useEffect(() => {
 
         if (!currUser) return nav('/');
-        let allWorkoutsArr = []
-
-        const getDefaultWorkouts = async () => {
-            try {
-                const q = query(
-                    collection(db, "DEFAULT_WORKOUTS"),
-                    orderBy("lastUsed")
-                )
-                const unsub = onSnapshot(q, querySnapshot => {
-                    querySnapshot.forEach(workout => {
-                        let data = workout.data();
-                        data["isDeletable"] = false
-                        data["id"] = workout.id
-                        allWorkoutsArr.push(data)
-                    })
-
-                })
-
-                return () => unsub
-
-            } catch (e) {
-                console.error(e)
-                alert("Error: could not read from database")
-            }
-
-        }
-
-        const getUserWorkouts = async () => {
-            // getDefaultWorkouts();
-            try {
-                const q = query(
-                    collection(db, 'users'),
-                    where('uid', '==', currUser?.uid)
-                );
-                console.log("here!")
-                const userDoc = await getDocs(q);
-                setUser(userDoc.docs[0].id)
-
-                if (user !== '') {
-                    const q = query(collection(db, 'users', user, 'workouts'), orderBy("lastUsed"))
-                    console.log("hello there")
-                    const unsub = onSnapshot(q, (querySnapshot) => {
-                        querySnapshot.forEach(workout => {
-                            let data = workout.data();
-                            data["isDeletable"] = true
-                            allWorkoutsArr.push(data);
-                        })
-                        allWorkoutsArr = sortArray(allWorkoutsArr)
-                        console.log(allWorkoutsArr)
-                        setAllWorkouts([...allWorkoutsArr])
-                    })
-
-                    return () => unsub
-                }
-
-            } catch (err) {
-                return;
-            }
-        }
-
-        getUserWorkouts()
         fetchUserName();
+        fetchDefaultWorkouts();
+    }, [currUser])
 
-    }, [])
+    // useEffect(() => {
+
+    //     if (!currUser) return nav('/');
+    //     let allWorkoutsArr = []
+
+
+    //     const getUserWorkouts = async () => {
+    //         try {
+    //             const q = query(
+    //                 collection(db, 'users'),
+    //                 where('uid', '==', currUser?.uid)
+    //             );
+    //             const userDoc = await getDocs(q);
+    //             setUser(userDoc.docs[0].id)
+
+    //             if (user !== undefined) {
+    //                 console.log("sdf") 
+    //                 const q = query(collection(db, 'users', user, 'workouts'))
+    //                 console.log("hello there")
+    //                 let tempData = []
+    //                 const unsub = onSnapshot(q, (querySnapshot) => {
+    //                     querySnapshot.forEach(workout => {
+    //                         let data = workout.data();
+    //                         data["isDeletable"] = true
+    //                         tempData.push(data);
+    //                     })
+
+    //                     console.log(tempData + " sdfs")
+    //                     setAllOfWorkouts([...tempData])
+    //                 })
+
+    //                 return () => unsub
+    //             }
+
+    //         } catch (err) {
+    //             return;
+    //         }
+    //     }
+
+    //     if(currUser && allOfWorkouts.length!== 0){
+    //         console.log(allOfWorkouts)
+    //         console.log(currUser) 
+    //         getUserWorkouts();
+    //         console.log(allOfWorkouts)
+    //     }
+
+    // }, [ currUser, allOfWorkouts])
 
     return (
         <div className="dashboard-holder">
@@ -145,9 +154,9 @@ export default function Dashboard() {
                     <WorkoutChooser />
                 </div>
                 <div className="bottom-half-holder">
-                    {allWorkouts.map(workout => {
+                    {allOfWorkouts.length !== 0 ? allOfWorkouts.map(workout => {
                         return <WorkoutSet name={workout.name} workout={workout.exercises} isDeletable={workout.isDeletable} docID={workout.id} />
-                    })}
+                    }): <></>}
                 </div>
             </div>
         </div >
